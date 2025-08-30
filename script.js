1e9dc530a390399e2bf9ae9a81941bebf068bb03
David <141168015+Ha-Khu@users.noreply.github.com>
2025-08-30 19:13:50 +0200

    Add localStorage for todo-list project

--- script.js
+++ script.js
@@ -2,17 +2,38 @@ const input = document.querySelector(".input-text");
 const add = document.querySelector(".add-btn");
 const list = document.querySelector("ul");
 
-add.addEventListener("click", () =>{
-  let inputValue = input.value;
+function saveTasks(){
+  let tasks = []
   
+  document.querySelectorAll("li").forEach(li =>{
+    const checkbox = li.querySelector("input[type='checkbox']");
+    const span = li.querySelector("span");
+
+    tasks.push({
+      text: span.textContent,
+      checked: checkbox.checked
+    });
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(){
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach(task =>{
+    addTask(task.text, task.checked);
+  })
+}
+
+function addTask(text, completed = false){
   const li = document.createElement("li");
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.classList.add("checkbox");
+  checkbox.checked = completed;
 
   const span = document.createElement("span");
-  span.textContent = inputValue;
+  span.textContent = text;
   span.classList.add("list-text");
 
   const deleteBtn = document.createElement("button");
@@ -21,14 +42,22 @@ add.addEventListener("click", () =>{
 
   deleteBtn.addEventListener("click", () =>{
     li.remove();
+    saveTasks()
   });
+
+  checkbox.addEventListener("change", saveTasks);
 
   li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(deleteBtn);
 
   list.appendChild(li);
+}
 
+add.addEventListener("click", () =>{
+  addTask(input.value);
+  saveTasks();
   input.value = "";
-
 });
+
+document.addEventListener("DOMContentLoaded", loadTasks);
